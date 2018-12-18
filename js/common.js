8596f5f6265eeae297870b515bfa4c4ee6d9f5a5
Vlad.Filinsky <vfilinsky.sells@gmail.com>
2018-12-18 12:15:18 +0500

    18.12

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
 $(function() {
-
-	// Custom JS
+	// Маска ввода номера телефона (плагин maskedinput)
+  $('[name="Телефон"]').mask("+7(999) 999-99-99");
+  // Модальное окно popup,с формой
+$('.popup-with-form').on('click', function(event) {
+  event.preventDefault();
+  $('.white-popup-form > .h4').html('заказать звонок');
+  $('.white-popup-form > button').html('заказать звонок');
+});
+$('.popup-with-form').magnificPopup({
+   type: 'inline',
+   focus: '#name',
+    removalDelay: 500,
+    mainClass: 'mfp-fade',
+});
 
 });
